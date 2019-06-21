7e20098ecdfe60a9515c6d1510c669185ffbe862
Deepak Pawar <pawardeepakv@gmail.com>
2019-06-21 17:43:46 +0530

    Commit from office

---
+++ app/src/main/java/user/simfi/com/myapplication/waysToDefineFragment/ActivityAddFragXML.java
@@ -0,0 +1,18 @@
+package user.simfi.com.myapplication.waysToDefineFragment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import user.simfi.com.myapplication.R;
+
+public class ActivityAddFragXML extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/user/simfi/com/myapplication/waysToDefineFragment/FragmentA.java
@@ -0,0 +1,18 @@
+package user.simfi.com.myapplication.waysToDefineFragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import user.simfi.com.myapplication.R;
+
+public class FragmentA extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a,container,false);
+    }
+}
